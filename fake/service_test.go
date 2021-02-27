d1c03f7b8ff4f8f3a79305f571f949c06ada053f
Alexandre <alr-lab@users.noreply.github.com>
2021-02-27 10:17:50 +0100

    Refine fake doubles (#5)

--- fake/service_test.go
+++ fake/service_test.go
@@ -2,19 +2,28 @@ package main
 
 import "testing"
 
-const expectedEmail = "fake"
+const (
+	emailDefault = ""
+	emailValidUser = "fake"
+)
 
 type FakeStore struct {}
 
 func (s FakeStore) GetCustomerEmail(id int) string {
-	return expectedEmail
+	email := emailDefault
+	switch id {
+	case 42:
+		email = emailValidUser
+	}
+
+	return email
 }
 
 func TestService_Get(t *testing.T) {
 	s := Service{store: FakeStore{}}
 
 	got := s.Get()
-	if got != expectedEmail {
-		t.Fatalf("got %q, want %q", got, expectedEmail)
+	if got != emailValidUser {
+		t.Fatalf("got %q, want %q", got, emailValidUser)
 	}
 }
