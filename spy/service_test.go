4cc403a2960c3c38632b25e1a5197639b9cc5229
Alexandre <alr-lab@users.noreply.github.com>
2021-02-27 14:31:35 +0100

    Add comments (#19)

--- spy/service_test.go
+++ spy/service_test.go
@@ -8,10 +8,18 @@ import (
 
 const email = "fake"
 
+// SpyStore describes a spying datastore, a datastore which allows us to
+// know if specific methods were called.
 type SpyStore struct {
 	getCustomerEmailCalled bool
 }
 
+// GetCustomerEmail returns a customer email.
+//
+// The spying method is recording that the production-ready method would
+// have been called. An alternative is to actually count the number of
+// times a method was called and expect it to be called a specific number
+// of times after the test execution.
 func (s *SpyStore) GetCustomerEmail(id int) string {
 	s.getCustomerEmailCalled = true
 	return email
