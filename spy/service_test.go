6c4f0e96a75817299b3be62bd2d6976757e5aaab
Alexandre <alr-lab@users.noreply.github.com>
2021-02-27 10:45:46 +0100

    Add spy (#8)

---
+++ spy/service_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/alr-lab/test-double/service"
+)
+
+const email = "fake"
+
+type SpyStore struct {
+	getCustomerEmailCalled bool
+}
+
+func (s *SpyStore) GetCustomerEmail(id int) string {
+	s.getCustomerEmailCalled = true
+	return email
+}
+
+func TestService_Get(t *testing.T) {
+	spy := SpyStore{}
+	serv := service.New(&spy)
+
+	got := serv.Get()
+	if got != email {
+		t.Fatalf("got %q, want %q", got, email)
+	}
+	if !spy.getCustomerEmailCalled {
+		t.Fatal("GetCustomerEmail not called")
+	}
+}
